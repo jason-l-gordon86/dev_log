40065c07c1800d8d54f9140f9e8a77f980d4fadc
Jason <jason.l.gordon86@gmail.com>
2015-09-29 12:02:56 +0000

    finished blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+  root to: 'articles#index'
 end
